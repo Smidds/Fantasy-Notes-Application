5949927f49b8b2ca5d9123ffd9a24c7d8001784a
Isaac Smith <isaac.smith@morningstar.com>
2019-07-11 14:16:35 -0500

    Use Quasar for frontend

---
+++ client/src/layouts/DashboardLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+    <q-header reveal class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="center">
+        <q-route-tab to="/page1" label="Personal" />
+        <q-route-tab to="/page2" label="Shared With Me" />
+        <q-route-tab to="/page3" label="Public" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "DashboardLayout"
+};
+</script>
+
+<style></style>
---
+++ client/src/pages/Auth/AuthWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="auth-container"></div>
+</template>
+
+<script>
+var firebase = require("firebase");
+var firebaseui = require("firebaseui");
+
+export default {
+  name: "AuthContainer"
+};
+</script>
+
+<style>
+</style>
